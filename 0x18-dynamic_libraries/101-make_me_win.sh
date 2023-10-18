c0680bfb89e0657d78197b524260a9d29cf30c5d
asmaagamal119 <asmaagamal1109@gmail.com>
2023-10-18 16:22:22 -0700

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P tmp https://github.com/asmaagamal119/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P tmp https://github.com/asmaagamal119/alx-low_level_programming
+/raw/master/0x18-dynamic_libraries/libgiga.so
 export LP_PRELOAD=/tmp/libgiga.so
