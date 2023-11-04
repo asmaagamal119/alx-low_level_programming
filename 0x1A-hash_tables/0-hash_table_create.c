e2e0281beb4354ce19f47bdfc816493439dba20c
asmaagamal119 <asmaagamal1109@gmail.com>
2023-11-03 18:04:26 -0700

    hhh

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	table->array = (hash_node_t **)calloc(table->size
+			, sizeof(hash_node_t *));
 	if (!table->array)
 		return (NULL);
 	for (; i < size; i++)
